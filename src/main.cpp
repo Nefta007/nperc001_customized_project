4a30f7bad1554ab79ac2b892b10ae3c73354b36d
Nefta007 <nperc001@ucr.edu>
2024-05-31 23:50:27 -0700

    added game logic

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ my own original work.
 #define F4_Sharp 5404//370
 #define G4_Sharp 4818//415
 
-#define NUM_TASKS 3 // TODO: Change to the number of tasks being used
+#define NUM_TASKS 4 // TODO: Change to the number of tasks being used
 int I_Want_Billions[45] = {C5_Sharp, C5_Sharp, C5_Sharp, C5_Sharp, C5_Sharp, E5_Flat, F5_Sharp, C5_Sharp, C5_Sharp, B4_Flat, A4_Flat, A4_Flat, G4_Sharp, F4_Sharp, F4_Sharp, F4_Sharp, A4_Flat, C5_Sharp, B4_Flat, A4_Flat, F4_Sharp, B4_Flat, C5_Sharp, C5_Sharp, C5_Sharp, C5_Sharp, C5_Sharp, C5_Sharp, E5_Flat, F5_Sharp, C5_Sharp, C5_Sharp, B4_Flat, A4_Flat, A4_Flat, G4_Sharp, F4_Sharp, F4_Sharp, F4_Sharp, A4_Flat, C5_Sharp, B4_Flat, A4_Flat, F4_Sharp, B4_Flat};
 int I_want_Time[45] = {1, 1, 1, 1, 1, 2, 1, 5, 4, 2, 4, 2, 4, 3, 3, 3, 3, 2, 2, 2, 2, 8, 1, 1, 1, 1, 1, 1, 2, 1, 2, 4, 1, 2, 1, 4, 2, 2, 2, 2, 2, 2,2 ,2 ,6};
 
@@ -44,14 +44,15 @@ unsigned char is_up;
 unsigned char is_down;
 unsigned char j;
 unsigned char i;
-unsigned char player_money;
+unsigned int player_money;
 unsigned char player_bet;
 unsigned char player_loan;
 unsigned char cardCount;
 unsigned char player_suit;
 unsigned char dealer_suit;
-unsigned char player_face;
-unsigned char dealer_face;
+unsigned int player_face;
+unsigned int dealer_face;
+volatile unsigned char is_bet;
 char *dealer_card;
 char *player_card;
 char *dealer_cardF;
@@ -71,6 +72,7 @@ typedef struct _task
 const unsigned long JS_Period = 100;
 const unsigned long Card_Period = 1000;
 const unsigned long Background_Period = 200;
+const unsigned long Bet_Period = 1000;
 
 const unsigned long Direction_Period = 500;
 const unsigned long GCD_PERIOD = findGCD(JS_Period, Card_Period); // TODO:Set the GCD Period
@@ -147,6 +149,9 @@ int TickFtn_back(int state);
 enum card_state{idle_card, card_deal, suit_state, value_state};
 int TickFtn_Card(int state);
 
+enum bet_state{idle_money, bet_state, loan_state};
+int TickFtn_Bet(int state);
+
 int main(void)
 {
     // TODO: initialize all your inputs and ouputs
@@ -197,6 +202,11 @@ int main(void)
     tasks[i].period = Card_Period;
     tasks[i].elapsedTime = tasks[i].period;
     tasks[i].TickFct = &TickFtn_Card;
+    i++;
+    tasks[i].state = idle_money;
+    tasks[i].period = Bet_Period;
+    tasks[i].elapsedTime = tasks[i].period;
+    tasks[i].TickFct = &TickFtn_Bet;
 
     TimerSet(GCD_PERIOD);
     TimerOn();
@@ -294,6 +304,7 @@ int TickFtn_Card(int state){
         dealer_face = 0;
         player_suit = 0;
         player_face = 0;
+        is_bet = 0;
         state = card_deal;
     break;
 
@@ -309,47 +320,12 @@ int TickFtn_Card(int state){
     break;
 
     case suit_state:
-        // if( dealer_suit == 1){
-        //     dealer_face = player_card[rand()%13];
-        // }
-        // else if(dealer_suit == 2){
-        //     dealer_face = player_card[rand()%13];
-        // }
-        // else if(dealer_suit == 3){
-        //     dealer_face = player_card[rand()%13];
-        // }
-        // else{
-        //     dealer_face = player_card[rand()%13];
-        // }
-        // if( player_suit == 1){
-        //     player_face = club_cards[rand()%13];
-        // }
-        // else if(player_suit == 2){
-        //     player_face = heart_cards[rand()%13];
-        // }
-        // else if(player_suit == 3){
-        //     player_face = diamond_cards[rand()%13];
-        // }
-        // else{
-        //     player_face = spade_cards[rand()%13];
-        // }
-        
+        dealer_face = card_values[rand()%13];
+        player_face = card_values[rand()%13];
         state = value_state;
     break;
 
     case value_state:
-        // if( dealer_suit == 1){
-        //     dealer_face = club_cards[rand()%13];
-        // }
-        // else if(dealer_suit == 2){
-        //     dealer_face = heart_cards[rand()%13];
-        // }
-        // else if(dealer_suit == 3){
-        //     dealer_face = diamond_cards[rand()%13];
-        // }
-        // else{
-        //     dealer_face = spade_cards[rand()%13];
-        // }
         state = idle_card;
     break;
 
@@ -363,15 +339,19 @@ int TickFtn_Card(int state){
     break;
 
     case card_deal:
-        serial_println(cardCount);
+        serial_println(dealer_cardF);
         serial_println(dealer_card);
-        // serial_println(dealer_face);
-        serial_println(player_suit);
-        // serial_println(player_face);
+        serial_println("-------------");
+        serial_println(player_cardF);
+        serial_println(player_card);
+        if(dealer_face > player_face){
+            serial_println("dealer wins!!");
+        }
+        else if(dealer_face < player_face){
+            serial_println("Player wins!!!");
+        }
     break;
 
-// value = club_cards[rand()%13];
-// serial_println(value);
 
     case suit_state:
         if( dealer_suit == 1){
@@ -401,7 +381,91 @@ int TickFtn_Card(int state){
     break;
 
     case value_state:
-
+    is_bet = 1;
+    if( dealer_face == 1){
+            dealer_cardF = "Ace";
+        }
+        else if(dealer_face == 2){
+            dealer_cardF = "2 of";
+        }
+        else if(dealer_face == 3){
+            dealer_cardF = "3 of";
+        }
+        else if(dealer_face == 4){
+            dealer_cardF = "4 of";
+        }
+        else if(dealer_face == 5){
+            dealer_cardF = "5 of";
+        }
+        else if(dealer_face == 6){
+            dealer_cardF = "6 of";
+        }
+        else if(dealer_face == 7){
+            dealer_cardF = "7 of";
+        }
+        else if(dealer_face == 8){
+            dealer_cardF = "8 of";
+        }
+        else if(dealer_face == 9){
+            dealer_cardF = "3 of";
+        }
+        else if(dealer_face == 10){
+            dealer_cardF = "10 of";
+        }
+        else if(dealer_face == 11){
+            dealer_cardF = "Jack of";
+        }
+        else if(dealer_face == 12){
+            dealer_cardF = "Queen of";
+        }
+        else if(dealer_face == 13){
+            dealer_cardF = "King of";
+        }
+        else{
+            dealer_cardF = "";
+        }
+        if( player_face == 1){
+            player_cardF = "Ace of";
+        }
+        else if(player_face == 2){
+            player_cardF = "2 of";
+        }
+        else if(player_face == 3){
+            player_cardF = "3 of";
+        }
+        else if(player_face == 4){
+            player_cardF = "4 of";
+        }
+        else if(player_face == 5){
+            player_cardF = "5 of";
+        }
+        else if(player_face == 6){
+            player_cardF = "6 of";
+        }
+        else if(player_face == 7){
+            player_cardF = "7 of";
+        }
+        else if(player_face == 8){
+            player_cardF = "8 of";
+        }
+        else if(player_face == 9){
+            player_cardF = "9 of";
+        }
+        else if(player_face == 10){
+            player_cardF = "10 of";
+        }
+        else if(player_face == 11){
+            player_cardF = "Jack of";
+        }
+        else if(player_face == 12){
+            player_cardF = "Queen of";
+        }
+        else if(player_face == 13){
+            player_cardF = "King of";
+        }
+        else{
+            player_cardF = "";
+        }
     break;
 
     default:
@@ -410,7 +474,6 @@ int TickFtn_Card(int state){
 
     return state;
 }
-
 
 // enum Back_state{idleMusic_state, note_state, play_state};
 int TickFtn_back(int state){
@@ -457,6 +520,56 @@ int TickFtn_back(int state){
         OCR1A = ICR1/2;
         j--;
     
+    default:
+        break;
+    }
+    return state;
+}
+
+// enum bet_state{idle_money, bet_state, loan_state};
+int TickFtn_Bet(int state){
+    switch (state)
+    {
+    case idle_money:
+        player_money = 1000;
+        player_bet = 0;
+        player_loan = 0;
+        state = bet_state;
+        break;
+    case bet_state:
+        if(player_money > 0 && is_bet == 1){
+            state = bet_state;
+        }
+        else{
+            state = loan_state;
+        }
+        break;
+    case loan_state:
+        if(player_money <= 0){
+            state = loan_state;
+        }
+        else{
+            state = bet_state;
+        }
+        break;
+    default:
+        break;
+    }
+    switch (state)
+    {
+    case idle_money:
+        break;
+    case bet_state:
+        serial_println("do you want to make a bet");
+        if(!((PINC >> 3)&0x01)){
+            serial_println("you bet 100 dollars");
+            player_money = player_money - 100;
+            serial_println(player_money);
+        }
+        break;
+    case loan_state:
+        serial_println("do you require a loan?");
+        break;
     default:
         break;
     }
